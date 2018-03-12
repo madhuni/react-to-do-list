7dceb39c3e068a8e6b7f29484836c63b783d4286
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-03-12 21:45:59 +0530

    Switched to Class based component from Functional component

--- src/components/ListItems/ListItem/ListItem.jsx
+++ src/components/ListItems/ListItem/ListItem.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const listItem = (props) => {
-  return (
-    <li className="list-item">{props.text}</li>
-  );
-};
+import './ListItem.css';
 
-export default listItem;
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.classes = ['list-item'];
+  }
+
+  render() {
+    return (
+      <li className={this.classes.join(' ')} onClick={() => this.props.deleteItem(this.props.keyValue)} >
+        <span className="item-text">{this.props.text}</span>
+      </li>
+    );
+  }
+}
+
+export default ListItem;
