356a0db9995c5a9895ef1d92f5c63ae923c08e27
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-03-12 21:50:55 +0530

    Add input placeholder and add props

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './InputForm.css';
 
 const inputForm = (props) => {
   return (
     <form onSubmit={props.addItems} id="input-form">
       <input
+        className="form-input"
         type="text"
+        placeholder="What's in your mind?"
         ref={(el) => {
           this._input = el;
           props.setRef(this._input);
