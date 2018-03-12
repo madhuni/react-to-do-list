7c0e071ba768647a15dd1ab3b6718068a5ff2076
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-03-12 12:48:24 +0530

    Initial setup for the new REACT application

---
+++ README.md
@@ -0,0 +1,4 @@
+## To-do-list on React
+
+This is a basic TO-DO-LIST application made on REACT JS.
+This project is simply to apply the basic concepts of REACT all together to build a small application.
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const destination = document.querySelector('#root');
+ReactDOM.render(
+  <div>
+    <h1>React is working fine!</h1>
+  </div>,
+  destination
+);
