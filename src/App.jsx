13dc792df69119072b38d9e3873bd45ff545bf64
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-03-12 22:58:34 +0530

    Add header component and more styles to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 import ToDoList from './containers/ToDoList';
+import Header from './components/Header/Header';
+import Aux from './hoc/Aux';
 
 class App extends Component {
   render() {
+    var styles = {
+      display: 'flex',
+      justifyContent: 'center',
+      marginBottom: '30px'
+    };
+
     return (
-      <div id="app">
-        <ToDoList />
-      </div>
+      <Aux>
+        <Header />
+        <div id="app" style={styles}>
+          <ToDoList />
+        </div>
+      </Aux>
     );
   }
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css';
+
+const header = (props) => {
+  return(
+    <header className="header">
+      <h1 className="header__heading">My To-Do-List</h1>
+    </header>
+  );
+}
+
+export default header;
